a75b7c75bcd85f005e9878126a94daec09e4eb1e
mohamed-jonah <mohamed.jnah01@gmail.com>
2025-05-09 12:07:08 +0100

    Implemented MenuOptions enum with SORT, SEARCH, ADD, GENERATE, and EXIT action

--- src/ca_2/MenuOptions.java
+++ src/ca_2/MenuOptions.java
@@ -8,6 +8,21 @@ package ca_2;
  *
  * @author Gerant
  */
-public class MenuOptions {
-    
+public enum MenuOptions {
+    SORT("Sort Employees"),
+    SEARCH("Search Employee"),
+    ADD("Add Employee"),
+    GENERATE("Generate Random"),
+    EXIT("Exit");
+
+    private final String displayText;
+
+    MenuOptions(String displayText) {
+        this.displayText = displayText;
+    }
+
+    @Override
+    public String toString() {
+        return displayText;
+    }
 }
